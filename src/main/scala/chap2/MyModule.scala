9a0989ff4892a5892d228dabe92f641b0e511413
Grégoire Neuville <gregoire.neuville@gmail.com>
2013-04-30 18:03:06 +0200

    Through chapter 2...

--- src/main/scala/chap2/MyModule.scala
+++ src/main/scala/chap2/MyModule.scala
@@ -1,10 +1,27 @@
 package fpis.chap2
 
+import scala.annotation._
+
 object MyModule {
   def abs(n: Int) =
     if (n < 0) -n else n
 
   def formatAbs(x: Int) =
     s"The absolute value of $x is ${abs(x)}"
+
+  def factorial(n: Int): Int = {
+    @tailrec
+    def go(x: Int, acc: Int): Int =
+      if (x <= 0) acc
+      else go(x - 1, acc * x)
+    go(n, 1)
+  }
+
+  def fib(n: Int): Int = {
+    def go(x: Int, prev: Int, cur: Int): Int =
+      if (x >= n) prev
+      else go(x + 1,  cur, prev + cur)
+    go(1, 0, 1)
+  }
 
 }
--- src/main/scala/chap2/Run.scala
+++ src/main/scala/chap2/Run.scala
@@ -7,5 +7,11 @@ object Run extends App {
   println("#### 2.2")
   println(formatAbs(-42))
 
+  println("#### 2.5")
+  println("Factorial")
+  println(factorial(4))
+  println("Fibonacci")
+  println(fib(7))
+
 }
 
